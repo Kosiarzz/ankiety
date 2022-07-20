9771c2112948e3933bddc7efca410d824446d852
PatrykP <patryk.pasciakk@gmail.com>
2022-07-20 16:17:25 +0200

    Create a migration (polls, questions, entries, answers)

---
+++ database/migrations/2022/2022_07_20_133506_create_polls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('polls', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->boolean('status')->default(false); //true = published, false = not published
+            $table->string('slug')->unique();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('polls');
+    }
+}
---
+++ database/migrations/2022/2022_07_20_133543_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->string('answer');
+            $table->enum('type', ['radio', 'text']);
+            $table->integer('order');
+            $table->timestamps();
+            $table->foreignId('poll_id')->constrained('polls')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
